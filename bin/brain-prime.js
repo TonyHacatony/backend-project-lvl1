5a7e84f85589815791481bb45c4902725ad1182b
EVeremenkov <hands.fins@gmail.com>
2022-07-17 15:45:55 +0300

    fixed brain-prime file

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -14,8 +14,9 @@ const createQuestionAndCorrectAnswer = () => {
 
 const isPrime = (number) => {
     for (let i = 2; i * i <= number; i++)
-        if (number % i === 0)
-          return false; 
+        if (number % i === 0) {
+            return false;
+        }
     return number > 1;
 }
 
